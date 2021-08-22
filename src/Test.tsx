a1253159cb3a8d9355beebf3664d514113bb951e
grimSkepsis <developer.mikehofer@gmail.com>
2021-08-22 12:07:59 -0700

    Adds tailwind css

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { ReactElement } from "react";
 
-const Test = (props: { test: string }) => {
-  return <div>test me {props.test}</div>;
+const Test = (props: { test: string }): ReactElement => {
+  return (
+    <div className="w-full text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+      test me {props.test}
+    </div>
+  );
 };
 
 export default Test;
